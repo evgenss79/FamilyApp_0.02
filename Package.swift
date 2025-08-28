29156a86672518fc5620dbcb070b88704f3cbd18
evgenss79 <evgenss79@gmail.com>
2025-08-28 04:17:25 -0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let packge = Package(
+    name: "FamilyApp",
+    platforms: [
+        .macOS(.v11)
+    ],
+    products: [
+        .executable(name: "FamilyApp", targets: ["FamilyApp"])
+    ],
+    dependencies: [],
+    targets: [
+        .executableTarget(
+            name: "FamilyApp",
+            path: "Sources/FamilyApp"
+        )
+    ]
+)
