7ec6ed449530a7a4d090b8cafdfe4dbcd40e420b
evgenss79 <evgenss79@gmail.com>
2025-08-28 05:44:47 -0700

    Create task.dart

---
+++ FamilyAppFlutter/lib/models/task.dart
@@ -0,0 +1,27 @@
+import 'package:uuid/uuid.dart';
+
+/// Model representing a task that can be assigned to one or more family members.
+class Task {
+  /// Unique identifier for the task.
+  final String id;
+
+  /// Title summarizing the task.
+  final String title;
+
+  /// Optional description providing more detail.
+  final String? description;
+
+  /// Optional due date and time when the task should be completed.
+  final DateTime? dueDate;
+
+  /// Identifier of the member assigned to the task, if any.
+  final String? assignedMemberId;
+
+  Task({
+    String? id,
+    required this.title,
+    this.description,
+    this.dueDate,
+    this.assignedMemberId,
+  }) : id = id ?? const Uuid().v4();
+}
