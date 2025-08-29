b75a7d3288fdd8e31ca7bc0a8b9a550e6e0fbbdf
evgenss79 <evgenss79@gmail.com>
2025-08-29 06:19:55 -0700

    Add main_v001.dart with schedule tab for version 0.01

---
+++ FamilyAppFlutter/lib/main_v001.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'services/storage_service_v001.dart';
+import 'providers/family_data_v001.dart';
+import 'screens/members_screen_v001.dart';
+import 'screens/tasks_screen_v001.dart';
+import 'screens/events_screen_v001.dart';
+import 'screens/schedule_screen_v001.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await StorageServiceV001.init();
+  final familyData = FamilyDataV001();
+  await familyData.loadFromStorage();
+  runApp(
+    ChangeNotifierProvider.value(
+      value: familyData,
+      child: const FamilyAppV001(),
+    ),
+  );
+}
+
+class FamilyAppV001 extends StatelessWidget {
+  const FamilyAppV001({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Family App',
+      home: const HomeTabsV001(),
+    );
+  }
+}
+
+class HomeTabsV001 extends StatefulWidget {
+  const HomeTabsV001({super.key});
+
+  @override
+  State<HomeTabsV001> createState() => _HomeTabsV001State();
+}
+
+class _HomeTabsV001State extends State<HomeTabsV001> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _screens = <Widget>[
+    MembersScreenV001(),
+    TasksScreenV001(),
+    EventsScreenV001(),
+    ScheduleScreenV001(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Members',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.check),
+            label: 'Tasks',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'Events',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Schedule',
+          ),
+        ],
+      ),
+    );
+  }
+}
