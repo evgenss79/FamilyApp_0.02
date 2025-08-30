5202ceba30e306d2233e80bd871aeb1203377432
evgenss79 <evgenss79@gmail.com>
2025-08-30 08:04:59 -0700

    Add Message model and adapter

---
+++ FamilyAppFlutter/lib/models/message.dart
@@ -0,0 +1,81 @@
+import 'package:hive/hive.dart';
+import 'package:uuid/uuid.dart';
+
+@HiveType(typeId: 5)
+class Message {
+  @HiveField(0)
+  final String id;
+  @HiveField(1)
+  final String conversationId;
+  @HiveField(2)
+  final String senderId;
+  @HiveField(3)
+  final String content;
+  @HiveField(4)
+  final DateTime timestamp;
+
+  Message({
+    String? id,
+    required this.conversationId,
+    required this.senderId,
+    required this.content,
+    DateTime? timestamp,
+  })  : id = id ?? const Uuid().v4(),
+        timestamp = timestamp ?? DateTime.now();
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'conversationId': conversationId,
+      'senderId': senderId,
+      'content': content,
+      'timestamp': timestamp.toIso8601String(),
+    };
+  }
+
+  static Message fromMap(Map<String, dynamic> map) {
+    return Message(
+      id: map['id'] as String?,
+      conversationId: map['conversationId'] as String,
+      senderId: map['senderId'] as String,
+      content: map['content'] as String,
+      timestamp: map['timestamp'] != null ? DateTime.parse(map['timestamp']) : DateTime.now(),
+    );
+  }
+}
+
+class MessageAdapter extends TypeAdapter<Message> {
+  @override
+  final int typeId = 5;
+
+  @override
+  Message read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (var i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Message(
+      id: fields[0] as String?,
+      conversationId: fields[1] as String,
+      senderId: fields[2] as String,
+      content: fields[3] as String,
+      timestamp: fields[4] as DateTime,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Message obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.conversationId)
+      ..writeByte(2)
+      ..write(obj.senderId)
+      ..writeByte(3)
+      ..write(obj.content)
+      ..writeByte(4)
+      ..write(obj.timestamp);
+  }
+}
