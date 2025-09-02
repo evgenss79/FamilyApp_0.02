febdc41b7d830747a683e90876e2d648e413e048
evgenss79 <evgenss79@gmail.com>
2025-09-02 04:49:43 -0700

    feat: expand NotificationService imports and init methods

--- FamilyAppFlutter/lib/services/notification_service.dart
+++ FamilyAppFlutter/lib/services/notification_service.dart
@@ -1,3 +1,21 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/foundation.dart';
 
-class NotificationService {}
+import '../models/task.dart';
+import '../providers/family_data.dart';
+import '../models/family_member.dart';
+
+class NotificationService {
+  static GlobalKey<ScaffoldMessengerState>? _scaffoldMessengerKey;
+  static BuildContext? _attachedContext;
+
+  static Future<void> init({GlobalKey<ScaffoldMessengerState>? scaffoldMessengerKey}) async {
+    _scaffoldMessengerKey = scaffoldMessengerKey;
+    // Additional initialization can be placed here.
+  }
+
+  static void attachContext(BuildContext context) {
+    _attachedContext = context;
+  }
+}
