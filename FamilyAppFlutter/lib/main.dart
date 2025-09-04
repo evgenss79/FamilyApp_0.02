0fd4e0141878231fcdcca049a344284d52a2c12b
evgenss79 <evgenss79@gmail.com>
2025-09-04 07:18:31 -0700

    fix(main): correct import statements

--- FamilyAppFlutter/lib/main.dart
+++ FamilyAppFlutter/lib/main.dart
@@ -1,4 +1,4 @@
-iimport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 mmport 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
