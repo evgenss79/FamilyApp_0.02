5aba3f2d8d34ca6823e0237989db366fc964e0b2
evgenss79 <evgenss79@gmail.com>
2025-09-04 18:24:05 +0200

    Update main.dart

--- FamilyAppFlutter/lib/main.dart
+++ FamilyAppFlutter/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-mmport 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
   import 'services/storage_service.dart';
