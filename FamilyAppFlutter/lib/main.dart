cde50940eda3d469014db8878a3a3f6aa555698e
evgenss79 <evgenss79@gmail.com>
2025-09-04 07:13:29 -0700

    feat(android): initialize Firebase in main.dart

--- FamilyAppFlutter/lib/main.dart
+++ FamilyAppFlutter/lib/main.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
+iimport 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'services/storage_service.dart';
+mimport 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+  import 'services/storage_service.dart';
 import 'providers/family_data.dart';
 import 'screens/members_screen.dart';
 import 'screens/tasks_screen.dart';
@@ -14,6 +17,8 @@ import 'screens/chat_list_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   await StorageServiceV001.init();
   await ChatStorageServiceV001.init();
   final familyData = FamilyDataV001();
