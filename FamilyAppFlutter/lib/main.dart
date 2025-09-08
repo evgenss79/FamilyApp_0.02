7d41c92ff01620aa04e05d874317a899e461b932
evgenss79 <evgenss79@gmail.com>
2025-09-08 13:42:25 +0200

    Добавлен import firebase_options.dart в main.dart и удалены случайные символы. Проверено, что Firebase.initializeApp и шифрование Hive инициализируются в service-init.Добавлен import firebase_options.dart в main.dart и удалены случайные символы. Проверено, что Firebase.initializeApp и шифрование Hive инициализируются в service-init.Добавлен импорт firebase_options.dart и удалены случайные символы, корректная инициализация Firebase сохранена.Удалены пустая строка и лишний символ '.' в начале chat_storage_svice.dart, код инициализации шифрования сохранен.

--- FamilyAppFlutter/lib/main.dart
+++ FamilyAppFlutter/lib/main.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+mport 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
