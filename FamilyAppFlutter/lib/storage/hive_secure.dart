746d995dd4871afe42b5af753391f26f5f63834e
evgenss79 <evgenss79@gmail.com>
2025-09-07 23:03:54 -0700

    Update hive_secure.dart to use consistent box names

--- FamilyAppFlutter/lib/storage/hive_secure.dart
+++ FamilyAppFlutter/lib/storage/hive_secure.dart
@@ -9,10 +9,10 @@ class HiveSecure {
     await store.ensureDek();
     final dek = await store.getDek();
     final cipher = HiveAesCipher(dek);
-    await Hive.openBox('family_members_v001', encryptionCipher: cipher);
-    await Hive.openBox('tasks_v001', encryptionCipher: cipher);
-    await Hive.openBox('events_v001', encryptionCipher: cipher);
-    await Hive.openBox('conversations_v001', encryptionCipher: cipher);
-    await Hive.openBox('messages_v001', encryptionCipher: cipher);
+    await Hive.openBox('familyMembersV001', encryptionCipher: cipher);
+    await Hive.openBox('taskV001', encryptionCipher: cipher);
+    await Hive.openBox('eventsV001', encryptioipher: cipher);
+    await Hive.openBox('conversationsV001', encryptionCipher: cipher);
+    await Hive.openBox('messagesV001', encryptionCipher: cipher);
   }
 }
