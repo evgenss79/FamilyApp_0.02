bccbfdf0e16909b920668bf58eca8f349b10f3b0
evgenss79 <evgenss79@gmail.com>
2025-09-10 04:17:05 -0700

    chore(models): comment out manual Hive adapter in chat.dart

--- FamilyAppFlutter/lib/models/chat.dart
+++ FamilyAppFlutter/lib/models/chat.dart
@@ -29,38 +29,39 @@ class Chat extends HiveObject {
 }
 
 // Manual adapter for Chat
-class ChatAdapter extends TypeAdapter<Chat> {
-  @override
-  final int typeId = 11;
-
-  @override
-  Chat read(BinaryReader reader) {
-    final numOfFields = reader.readByte();
-    final fields = <int, dynamic>{
-      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
-    };
-    return Chat(
-      id: fields[0] as String,
-      title: fields[1] as String,
-      memberIds: (fields[2] as List).cast<String>(),
-      updatedAt: fields[3] as DateTime,
-      lastMessagePreview: fields[4] as String?,
-    );
-  }
+////class ChatAdapter extends TypeAdapter<Chat> {
+//  @override
+//  final int typeId = 11;
+//
+//  @override
+//  Chat read(BinaryReader reader) {
+//    final numOfFields = reader.readByte();
+//    final fields = <int, dynamic>{
+//      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+//    };
+//    return Chat(
+//      id: fields[0] as String,
+//      title: fields[1] as String,
+//      memberIds: (fields[2] as List).cast<String>(),
+//      updatedAt: fields[3] as DateTime,
+//      lastMessagePreview: fields[4] as String?,
+//    );
+/  }
 
   @override
   void write(BinaryWriter writer, Chat obj) {
     writer
       ..writeByte(5)
-      ..writeByte(0)
-      ..write(obj.id)
-      ..writeByte(1)
-      ..write(obj.title)
-      ..writeByte(2)
-      ..write(obj.memberIds)
-      ..writeByte(3)
-      ..write(obj.updatedAt)
-      ..writeByte(4)
-      ..write(obj.lastMessagePreview);
-  }
-}
+//      ..writeByte(0)
+//      ..write(obj.id)
+//      ..writeByte(1)
+//      ..write(obj.title)
+//      ..writeByte(2)
+//      ..write(obj.memberIds)
+//      ..writeByte(3)
+//      ..write(obj.updatedAt)
+//      ..writeByte(4)
+//      ..write(obj.lastMessagePreview);
+//  }
+//}
+//
