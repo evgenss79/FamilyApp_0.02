7bc04a0b39f08c38866ecaf635a484f8d49daa90
evgenss79 <evgenss79@gmail.com>
2025-09-12 06:40:03 -0700

    fix: correct fallback member relationship in call_screen.dart

--- FamilyAppFlutter/lib/screens/call_screen.dart
+++ FamilyAppFlutter/lib/screens/call_screen.dart
@@ -29,8 +29,7 @@ class CallScreen extends StatelessWidget {
     final participants = conversation.memberIds
         .map((id) => familyData.members.firstWhere(
               (member) => member.id == id,
-              orElse: () => FamilyMember(id: '', name: 'Unknown, relationship: ''),
-            ))
+              orElse: () => FamilyMember(id: '', name: 'Unknown', relationship: ''),            ))
         .toList();
 
     // Capitalize call type for display.
