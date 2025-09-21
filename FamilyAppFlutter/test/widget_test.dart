9186bbee30dfc63c17d1cf33026e908b70874c51
evgenss79 <evgenss79@gmail.com>
2025-09-21 22:03:17 +0200

    Update widget_test.dart

--- FamilyAppFlutter/test/widget_test.dart
+++ FamilyAppFlutter/test/widget_test.dart
@@ -1,23 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:family_app_flutter/main.dart';
+import 'package:FamilyAppFlutter/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const FamilyAppV001());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('App smoke test', (WidgetTester tester) async {
+    // Загружаем корневой виджет
+    await tester.pumpWidget(MyApp());
   });
 }
