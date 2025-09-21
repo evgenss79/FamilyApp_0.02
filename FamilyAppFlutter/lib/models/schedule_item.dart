40375ca72101d6838a06f9376d372fe04156f37f
evgenss79 <evgenss79@gmail.com>
2025-09-21 21:32:34 +0200

    Update schedule_item.dart

--- FamilyAppFlutter/lib/models/schedule_item.dart
+++ FamilyAppFlutter/lib/models/schedule_item.dart
@@ -1,10 +1,62 @@
-/// Represents a generic item in a schedule view.  A [ScheduleItem]
-/// could correspond to an event, task or appointment.  Only an id,
-/// title and time are provided here for simplicity.
 class ScheduleItem {
-  final String? id;
-  final String? title;
-  final DateTime? dateTime;
+  final String id;
+  final String title;
+  final DateTime startDateTime;
+  final DateTime? endDateTime;
+  final String? description;
 
-  ScheduleItem({this.id, this.title, this.dateTime});
+  ScheduleItem({
+    required this.id,
+    required this.title,
+    required this.startDateTime,
+    this.endDateTime,
+    this.description,
+  });
+
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'title': title,
+        'startDateTime': startDateTime.toIso8601String(),
+        'endDateTime': endDateTime?.toIso8601String(),
+        'description': description,
+      };
+
+  static ScheduleItem fromMap(Map<String, dynamic> m) => ScheduleItem(
+        id: (m['id'] ?? '').toString(),
+        title: (m['title'] ?? '').toString(),
+        startDateTime: m['startDateTime'] is String ? DateTime.parse(m['startDateTime']) : DateTime.now(),
+        endDateTime: m['endDateTime'] is String ? DateTime.tryParse(m['endDateTime']) : null,
+        description: m['description'] as String?,
+      );
+}
+class ScheduleItem {
+  final String id;
+  final String title;
+  final DateTime startDateTime;
+  final DateTime? endDateTime;
+  final String? description;
+
+  ScheduleItem({
+    required this.id,
+    required this.title,
+    required this.startDateTime,
+    this.endDateTime,
+    this.description,
+  });
+
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'title': title,
+        'startDateTime': startDateTime.toIso8601String(),
+        'endDateTime': endDateTime?.toIso8601String(),
+        'description': description,
+      };
+
+  static ScheduleItem fromMap(Map<String, dynamic> m) => ScheduleItem(
+        id: (m['id'] ?? '').toString(),
+        title: (m['title'] ?? '').toString(),
+        startDateTime: m['startDateTime'] is String ? DateTime.parse(m['startDateTime']) : DateTime.now(),
+        endDateTime: m['endDateTime'] is String ? DateTime.tryParse(m['endDateTime']) : null,
+        description: m['description'] as String?,
+      );
 }
