abd981da693a0ae6ab42f82a48d94a965afab148
evgenss79 <evgenss79@gmail.com>
2025-09-21 19:37:46 +0200

    Update add_task_screen.dart

--- FamilyAppFlutter/lib/screens/add_task_screen.dart
+++ FamilyAppFlutter/lib/screens/add_task_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
-
-import '../models/task.dart';
-import '../models/family_member.dart';
 import '../providers/family_data.dart';
 import '../services/notification_service.dart';
 
