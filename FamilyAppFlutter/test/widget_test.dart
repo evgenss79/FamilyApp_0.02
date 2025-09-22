a70a0fa19938473b978728c86081aac50638116f
evgenss79 <evgenss79@gmail.com>
2025-09-22 14:11:09 +0200

    Fix macOS plugin registration and test harness

--- FamilyAppFlutter/test/widget_test.dart
+++ FamilyAppFlutter/test/widget_test.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:FamilyAppFlutter/main.dart';
+import 'package:family_app_flutter/main.dart';
+import 'package:family_app_flutter/providers/chat_provider.dart';
 
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
-    // Загружаем корневой виджет
-    await tester.pumpWidget(FamilyApp());
+    // Создаём минимальный экземпляр приложения с пустым ChatProvider.
+    final chatProvider = ChatProvider();
+    await tester.pumpWidget(MyApp(chatProvider: chatProvider));
   });
 }
