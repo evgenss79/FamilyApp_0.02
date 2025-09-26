6a1d35af1d711b58bd8de7097def64d31a65794c
evgenss79 <evgenss79@gmail.com>
2025-09-26 16:37:21 +0200

    Revert "feat: enable android webrtc calling"

--- FamilyAppFlutter/lib/services/notifications_service.dart
+++ FamilyAppFlutter/lib/services/notifications_service.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 import '../firebase_options.dart';
 import '../storage/local_store.dart';
 
