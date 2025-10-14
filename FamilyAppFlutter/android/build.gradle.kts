fb104c9b2178bcca62acfd5302a7a8181361a6f5
evgenss79 <evgenss79@gmail.com>
2025-10-14 14:40:41 +0200

    Remove redundant project repositories configuration

--- FamilyAppFlutter/android/build.gradle.kts
+++ FamilyAppFlutter/android/build.gradle.kts
@@ -4,10 +4,3 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
