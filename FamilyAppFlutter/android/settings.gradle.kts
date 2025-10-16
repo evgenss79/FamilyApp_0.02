68be677dcc3851263d7cfa83c8399d3addf136ba
evgenss79 <evgenss79@gmail.com>
2025-10-16 19:39:28 +0200

    fix(android): unify plugin resolution, remove duplicate classpaths, bump Gradle wrapper for AGP 8.9.1

--- FamilyAppFlutter/android/settings.gradle.kts
+++ FamilyAppFlutter/android/settings.gradle.kts
@@ -1,41 +1,24 @@
-// FamilyAppFlutter/android/settings.gradle.kts
-
 pluginManagement {
     repositories {
-        // Local plugin shelf: allows offline Gradle builds when jars are present in android/gradle/plugins.
-        flatDir {
-            dirs = setOf(file("gradle/plugins"))
-        }
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
     }
     plugins {
         id("com.android.application") version "8.9.1"
         id("org.jetbrains.kotlin.android") version "1.9.24"
-        // The Flutter Gradle plugin version is provided by the Flutter SDK.
-    }
-    resolutionStrategy {
-        eachPlugin {
-            when (requested.id.id) {
-                "org.jetbrains.kotlin.android" ->
-                    useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
-                "com.android.application" ->
-                    useModule("com.android.tools.build:gradle:8.9.1")
-                "com.google.gms.google-services" ->
-                    useModule("com.google.gms:google-services:4.4.2")
-            }
-        }
+        id("com.google.gms.google-services") version "4.4.2"
+        id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     }
 }
 
+plugins {
+    id("dev.flutter.flutter-plugin-loader")
+}
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        flatDir {
-            dirs = setOf(file("gradle/plugins"))
-        }
         google()
         mavenCentral()
         maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
